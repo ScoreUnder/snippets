abdceec4e6bef61559b6faa43298824acbc925c0
Score_Under <seejay.11@gmail.com>
2015-10-18 04:03:33 +0100

    Use err.h in setuid

--- setuid.c
+++ setuid.c
@@ -2,26 +2,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-
-#define err(x) write(2,(x),sizeof(x))
+#include<err.h>
 
 int main(int argc, char **argv)
 {
-    if(argc < 3) {
-        err("Need more args\n");
-        exit(1);
-    }
+    if(argc < 3) errx(1, "Need more args");
     char *end;
     int uid = strtol(argv[1], &end, 10);
-    if (*end || !*argv[1]) {
-        err("Bad UID format\n");
-        exit(1);
-    }
-    if (setuid(uid)) {
-        perror("Can't setuid");
-        exit(2);
-    }
+    if (*end || !*argv[1]) errx(1, "Bad UID format\n");
+    if (setuid(uid)) err(2, "Can't setuid");
     execve(argv[2], argv + 2, NULL);
-    perror("Exec failed");
-    exit(3);
+    err(3, "Exec failed");
 }
