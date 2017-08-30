425c9bbce026d578cef5157a0b9eb22a047ee502
Score_Under <seejay.11@gmail.com>
2017-08-30 12:57:53 +0100

    Another obfuscated thing I had lying around

---
+++ hw3.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+from __future__ import print_function
+(lambda q:((lambda p:((lambda u:(u(6,p,lambda l,d:l[-1]+d[1])(43579),u(7,q,lambda l,d:p[d[1]])(21994345)))(lambda n,p,c:(lambda f:f(f))(lambda f:lambda z:f(f)((lambda l,d:(l.append(c(l,d)),d[0])[1])(p,divmod(z,n)))if z else 0)),q.append(p[0])))([68]),q.insert(*(lambda x:(x,2**x))(5)),print(' '.join(map(str.capitalize,''.join(map(chr,q)).split())))))([])
